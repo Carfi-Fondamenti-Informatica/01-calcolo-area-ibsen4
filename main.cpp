60ff810bb437316b6bd693768b308f7324104f5c
ibsen4 <115497034+ibsen4@users.noreply.github.com>
2022-11-09 19:26:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main(){
-int a=0.0, b=0.0, c=0.0
+int a=0.0, b=0.0, c=0.0;
 cin >> a >> b >> c;
 cout<< a*b/2 << endl;
 cout<< 4*a << endl;
 cout<< (a+b)*2;
-cout<< (a+b)*c/2
+cout<< (a+b)*c/2;
 return 0
 }
